ccff56f87f97049cad78cea60ce1d63368b4ca02
tdwright <tom@tdwright.co.uk>
2018-12-10 22:08:33 +1100

    Example of a simple class file with some StyleCop messages

---
+++ Class1.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+
+namespace CodeAnalysersExampleProj
+{
+    public class Class1 {
+
+        public int MyProperty {get;set;}    // incorrect spacing
+        public int another_property {get;set;}    // incorrect spacing
+    
+}}
