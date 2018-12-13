88d9486f2a95c170b857852ab8475afbd2d636cb
tdwright <tom@tdwright.co.uk>
2018-12-13 19:11:34 +1100

    Fixed SA1002 (semicolons must be followed by a space)

--- Class1.cs
+++ Class1.cs
@@ -5,7 +5,7 @@ namespace CodeAnalysersExampleProj
 {
     public class Class1 {
 
-        public int MyProperty {get;set;}    // incorrect spacing
-        public int another_property {get;set;}    // incorrect spacing
+        public int MyProperty {get; set; }    // incorrect spacing
+        public int another_property {get; set; }    // incorrect spacing
     
 }}
